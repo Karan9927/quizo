b2a2c527ac9856dac9c8c803e7974043a6cc56e5
karan9927 <karanexcel.ks7417@gmail.com>
2025-02-15 11:29:03 +0530

    update

--- quizo-backend/src/server.ts
+++ quizo-backend/src/server.ts
@@ -10,10 +10,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors({ origin: "*" }));
 
 connectDB();
@@ -32,7 +30,4 @@ app.use(
   }
 );
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
