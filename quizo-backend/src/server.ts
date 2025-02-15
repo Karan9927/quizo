7e027785f911a75e8f8c9be45f837471aa178052
karan9927 <karanexcel.ks7417@gmail.com>
2025-02-15 16:11:04 +0530

    port updated

--- quizo-backend/src/server.ts
+++ quizo-backend/src/server.ts
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "https://quizo-frontend-eight.vercel.app",
-      "http://localhost:5174",
+      "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
